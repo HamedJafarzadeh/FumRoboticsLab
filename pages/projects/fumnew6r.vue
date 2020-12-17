e8d22028c51f30206b70cd419a611804d14d81c3
soroushatashin <s.atasheen@yahoo.com>
2020-12-17 19:03:42 +0330

    video for 6r is added

--- pages/projects/fumnew6r.vue
+++ pages/projects/fumnew6r.vue
@@ -6,14 +6,14 @@
       <br>
       <div class="row">
         <div class="col-12 justify-content-center text-center mx-auto my-auto" >
-          <img src="../../assets/ProjectPictures/FUM-New6R.jpg" style="width: 20% ; border-radius: 20%">
+          <img src="../../assets/ProjectPictures/FUM-New6R.jpg" style="width: 30% ; border-radius: 20%">
         </div>
       </div>
       <br>
       <br>
       <div class="row">
         <p>
-          Designed for various operations such as pick and place, packaging, and welding, this 6 DOF articulated arm has a maximum reach of 1600mm with a maximum payload of 16 kg. Maximum Cartesian speed reaches up to 3 m/s along the selected axis, with a repeatability of ±0.05mm. Open control architecture enables ease of use in research applications.
+          Designed for various operations such as pick and place, packaging, and welding. This 6 DOF articulated arm has a maximum reach of 1600mm with a maximum payload of 16 kg. Maximum Cartesian speed reaches up to 3 m/s along the selected axis, with a repeatability of ±0.05mm. Open control architecture enables ease of use in research applications.
         </p>
       </div>
 
