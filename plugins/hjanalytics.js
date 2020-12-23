47be9b16cdf48bf447cbe55912f151791205a168
Hamed <hjafarzadeh511@gmail.com>
2020-12-23 19:54:07 +0000

    Activate HjAnalytics

--- plugins/hjanalytics.js
+++ plugins/hjanalytics.js
@@ -1,8 +1,7 @@
 export default async ({ app }) => {
   app.router.afterEach(async(to, from) => {
-    console.log("Requesting to bot");
     app.$axios.post('http://server.hamedj.ir:2688/BotGateway/Log',
-      `logLevel=BlogVisit&logText=${to.path}`,
+      `logLevel=FUMRoboticsVisit&logText=${to.path}`,
       {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
