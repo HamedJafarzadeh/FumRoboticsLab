db860d8f0dbf6a126b3776c238bad183a964ec20
soroushatashin <s.atasheen@yahoo.com>
2020-12-26 12:44:20 +0330

    mobile platform and new people is added

--- pages/people.vue
+++ pages/people.vue
@@ -4,10 +4,10 @@
       <br>
       <br>
       <div class="row">
-        <div class="col-4 justify-content-center text-center mx-auto my-auto" >
+        <div class="col-lg-4 justify-content-center text-center mx-auto my-auto" >
           <img src="../assets/people/ali_akbarzadeh.jpg" style="width: 250px; border-radius: 20%">
         </div>
-        <div class="col-8">
+        <div class="col-lg-8">
           <h3>Alireza Akbarzadeh Tootoonchi</h3>
           <h4>Director of Advanced Robotics Lab</h4>
           <h5>Founding member of the Center of Excellence on Soft Computing and Intelligent Information Processing</h5>
@@ -33,7 +33,7 @@
 
 
       <div class="row text-center my-4">
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/mohammadreza_akbarzadeh.jpg" style="width: 300px; border-radius: 20%">
           <h5>Mohammadreza Akbarzadeh Tootoonchi</h5>
           <h5>akbarzadeh@ieee.org</h5>
@@ -45,7 +45,7 @@
             <h6 >Research Interests/Specializations: Fuzzy Logic, Artificial Inteligence, Soft Computing, Complex Systems, Biomedical Engineering  </h6>
 
         </div>
-        <div class="col-4"> <img src="../assets/people/kardan.jpg" style="width: 157px; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/kardan.jpg" style="width: 157px; border-radius: 20%">
           <h5>Iman Kardan</h5>
           <h5>iman.kardan@um.ac.ir</h5>
           <h6 >Assistant Professor </h6>
@@ -54,7 +54,7 @@
           <h6>Ferdowsi University Of Mashhad (FUM)</h6>
           <h6>Research Interests/Specializations: Rehabilitation Robotics, Human-Robot Interaction, Robot Control </h6>
         </div>
-        <div class="col-4"> <img src="../assets/people/alimoradi.jpg" style="width: 157px; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/alimoradi.jpg" style="width: 157px; border-radius: 20%">
           <h5>Ali Moradi</h5>
           <h5>almor0012@gmail.com</h5>
           <h6 >Associate Professor </h6>
@@ -70,7 +70,7 @@
 
 
       <div class="row text-center my-4">
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/negahban.jpg" style="width: 160px; border-radius: 20%">
           <h5>Hossein Negahban</h5>
           <h5>honegahban@yahoo.com</h5>
@@ -82,7 +82,7 @@
           <h6 >Research Interests/Specializations: Dual-Task, Outcome Measurment, Postural control, Musculoskeletal, Posture  </h6>
 
         </div>
-        <div class="col-4"> <img src="../assets/people/kamalh.jpg" style="width: 157px; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/kamalh.jpg" style="width: 157px; border-radius: 20%">
           <h5>Kamal Hosseini Sani</h5>
           <h5>iman.kardan@um.ac.ir</h5>
           <h6 >Assistant Professor </h6>
@@ -107,7 +107,7 @@
 
 
       <div class="row text-center my-4">
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/Alishiva.jpg" style="width: 157px; border-radius: 20%">
           <h5>Ali Shiva</h5>
           <h5>foroudalishiva@yahoo.com</h5>
@@ -119,7 +119,7 @@
             <h6 >Research Interests/Specializations: Robotics, Soft Robotics, Dynamics, Multi Body Dynamics, Control Engineering, Morphological Computation </h6>
 
         </div>
-        <div class="col-4"> <img src="../assets/people/tahami.jpg" style="width: 236px; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/tahami.jpg" style="width: 236px; border-radius: 20%">
           <h5>Seyyed Mohammad Tahamipour</h5>
           <h5>tahamipourzarandi@mail.um.ac.ir</h5>
 
@@ -127,7 +127,7 @@
           <h6>B.Sc. Electrical Engineering, Iran university of Science & Technology, Tehran, Iran, <br> (2012-2016)</h6>
           <h6>Research Interests/Specializations: Robotics, Rehabilitation Robots, Nonlinear Control Application, Artificial Inteligence </h6>
         </div>
-        <div class="col-4"> <img src="../assets/people/aminian.png" style="width: 44%; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/aminian.png" style="width: 44%; border-radius: 20%">
           <h5>Ebrahim Aminian</h5>
           <h5>ebrahimaminian_phd@yahoo.com  </h5>
           <h6>M.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2014-2017)</h6>
@@ -142,18 +142,18 @@
       <hr/>
 
       <div class="row text-center my-4">
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/amini.png" style="width: 240px; border-radius: 20%">
           <h5>Samane Amini</h5>
           <h5>amini.samane@ymail.com</h5>
 
-          <h6>Ph.D. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2014-2019)</h6>
-          <h6>M.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, (2002-2004)</h6>
+          <h6>Ph.D. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2017/present)</h6>
+          <h6>M.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, (2011-2013)</h6>
 
           <h6 >Research Interests/Specializations: Medical and Rehabilitation Robotics, Bio-Inspired Robot, Assistive control, Dynamic and Control Humanoid Robots, Assistive Devices </h6>
 
         </div>
-        <div class="col-4"> <img src="../assets/people/khaniki.png" style="width: 239px; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/khaniki.png" style="width: 239px; border-radius: 20%">
           <h5>Omid Khaniki</h5>
           <h5>Omid.Khaniki@mail.um.ac.ir</h5>
 
@@ -161,7 +161,7 @@
           <h6>B.Sc. Electrical Engineering, Ferdowsi university, Mashhad, Iran, <br> (2016-2019)</h6>
           <h6>Research Interests/Specializations: Optimization, Neuromusculoskeletal Modeling, Robotics, BioRobotics ,Rehabilitation, Artificial Intelligence, Digital Twin, Machine Design  </h6>
         </div>
-        <div class="col-4"> <img src="../assets/people/mogharebi.png" style="width: 43%; border-radius: 20%">
+        <div class="col-lg-4"> <img src="../assets/people/mogharebi.png" style="width: 43%; border-radius: 20%">
           <h5>Mostafa Moqarabi</h5>
           <h5>Mostafa.Mogharabi@gmail.com  </h5>
           <h6>Ph.D. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, (2018/present)</h6>
@@ -179,7 +179,7 @@
 
       <div class="row text-center my-4">
 
-    <div class="col-4">
+    <div class="col-lg-4">
       <img src="../assets/people/malekzade.jpg" style="width: 190px; border-radius: 20%">
       <h5>Mohammad Malekzadeh Arasteh</h5>
       <h5>Mohammad.Malek132@gmail.com  </h5>
@@ -188,7 +188,7 @@
       <h6>B.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2013-2017)</h6>
       <h6>Research Interests/Specializations: Humanoid Robotics, Legged Robotics, Continuous Passive Motion and Controlled Active Motion Devices  </h6>
     </div>
-      <div class="col-4"> <img
+      <div class="col-lg-4"> <img
         <img src="../assets/people/zare.png" style="width: 133px; border-radius: 20%">
         <h5>Amir Hossein Zare</h5>
         <h5>amir.h.zare1991@gmail.com </h5>
@@ -197,7 +197,7 @@
         <h6></h6>
         <h6>Research Interests/Specializations: Machine Learning, Reinforcement Learning, Vision, Programming, Control, Advanced Robotics </h6>
       </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/overlay.png" style="width: 195px; border-radius: 20%">
           <h5>Mohammad Rasool Vaezi</h5>
           <h5>mr_vaezi@mail.um.ac.ir</h5>
@@ -212,7 +212,7 @@
 
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/mosadegh.png" style="width: 140px; border-radius: 20%">
           <h5>Hamidreza Mosaddegh Hesar</h5>
           <h5>hamid.mosaddegh@yahoo.com  </h5>
@@ -221,7 +221,7 @@
           <h6>B.Sc. Electrical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2006-2011)</h6>
           <h6>Research Interests/Specializations: Variable-Speed AC Drives, Power Electronics, Electrical Machine Design, Nonlinear Control, Renewable Energy Technologies </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/razavi.png" style="width: 145px; border-radius: 20%">
           <h5>Seyyed Mohammadreza Razavi</h5>
           <h5>smreza_razavi@yahoo.com </h5>
@@ -230,7 +230,7 @@
           <h6>B.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2010-2014)</h6>
           <h6>Research Interests/Specializations: Optimal Control, Robust Control, State/Disturbance Observer, Adaptive Control </h6>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/makhdoomi.png" style="width: 175px; border-radius: 20%">
           <h5>Arian Makhdoomi</h5>
           <h5>makhdumiaryan@gmail.com</h5>
@@ -243,7 +243,7 @@
     <hr/>
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/atashin.png" style="width: 198px; border-radius: 20%">
           <h5>Soroush Ahooye Atashin</h5>
           <h5>s.atasheen@yahoo.com </h5>
@@ -251,7 +251,7 @@
           <h6>B.Sc. Electrical Engineering, Birjand University, Birjand, Iran, <br> (2012-2016)</h6>
           <h6>Research Interests/Specializations: Control Drives, Power Electronics, Electrical Machine Design </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/forutan.png" style="width: 185px; border-radius: 20%">
           <h5>Ali Forutanian</h5>
           <h5>aliforoutanni@gmail.com</h5>
@@ -259,7 +259,7 @@
           <h6>B.Sc. Mechanical Engineering, University of Neyshabur, Neyshabur, Iran, <br> (2014-2018)</h6>
           <h6>Research Interests/Specializations: Neuroimaging, Brain Connectivity, Cognitive Process, Brain Functional Modelling, linguistics Processing, Biomedical Signal Processing, Complex Systems, Nonlinear Dynamics, Chaotic System, Artificial Intelligence, </h6>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/maleki.png" style="width: 170px; border-radius: 20%">
           <h5>AmirKhosro Maleki</h5>
           <h5>maleki.amirkhosro@mail.um.ac.ir</h5>
@@ -272,7 +272,7 @@
       <hr/>
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/shahabi.png" style="width: 245px; border-radius: 20%">
           <h5>Mohammad Entekhab Shahabi</h5>
           <h5>m.en.shahabi@gmail.com </h5>
@@ -280,7 +280,7 @@
           <h6>B.Sc. Mechanical Engineering, Islamic Azad University, Mashhad, Iran, <br> (2006-2011)</h6>
           <h6>Research Interests/Specializations: Mechanical Design, Robotics, Robot-Assisted Physiotherapy and Robotic Rehabilitation, Flexible-Link Robots, Impedance Control, Fuzzy Control, Dynamic Simulation, Vibration Control, Artificial Neural Networks </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/soltani.png" style="width: 175px; border-radius: 20%">
           <h5>Arezoo Soltani</h5>
           <h5>Arezooslt49@gmail.com</h5>
@@ -288,7 +288,7 @@
           <h6>B.Sc. Mechanical Engineering, Islamic Azad University, Mashhad, <br> (2014-2018)</h6>
           <h6>Research Interests/Specializations: Neurorehabilitation, Biological Signal Processing, Artificial Neural Network </h6>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/eskandari.png" style="width: 207px; border-radius: 20%">
           <h5>Mohammad Hassan Eskandari</h5>
           <h5>mh_eskandari@outlook.com</h5>
@@ -301,7 +301,7 @@
       <hr/>
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/gholamian.png" style="width: 210px; border-radius: 20%">
           <h5>Farzaneh Gholamian</h5>
           <h5>F_gholamian@sut.ac.ir </h5>
@@ -309,14 +309,14 @@
           <h6>B.Sc. Orthotics & Prosthetics, . Iran University of Medical Sciences, Tehran, Iran, <br> (2011-2015)</h6>
           <h6>Research Interests/Specializations: Rehabilitation – bone biomechanics – finite element analysis - Implants </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/tayarani.png" style="width: 220px; border-radius: 20%">
           <h5>Keyvan Tayaranian Marvian</h5>
           <h5>k.tayaranianmarvian@mail.um.ac.ir</h5>
           <h6>B.Sc. Electrical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2016/present)</h6>
           <h6>Research Interests/Specializations: Control Theory, Rehabilitation Robotics, Machine Learning, and Computational Intelligence </h6>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/salehi.png" style="width: 150px; border-radius: 20%">
           <h5>Mostafa Salehi</h5>
           <h5>mostafa73salehi@gmail.com</h5>
@@ -334,21 +334,21 @@
       <hr/>
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/kohandel.png" style="width: 135px; border-radius: 20%">
           <h5>Reza Kohandel</h5>
           <h5>rezakohandel9@gmail.com </h5>
           <h6>B.Sc. Electrical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2017/present)</h6>
           <h6>Skills: Electronics & Biomedical Circuit Design – MATLAB – C – Circuits Simulation –Microcontrollers – PCB Design   </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/mohamadzade.png" style="width: 190px; border-radius: 20%">
           <h5>Amir Mohammad Mohammad Zadeh</h5>
           <h5>amirmohammad.m@hotmail.com</h5>
           <h6>B.Sc. Electrical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2017/present)</h6>
           <h6>Skills: Electronics & Power Electronics Circuit Design – MATLAB – C Programming– STM32 Microcontroller – PCB Design   </h6>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/Kazemi.png" style="width: 170px; border-radius: 20%">
           <h5>Mahdi KazemiEsfahani</h5>
           <h5>m.kazemiesfahani@mail.um.ac.ir</h5>
@@ -360,14 +360,14 @@
       <hr/>
       <div class="row text-center my-4">
 
-        <div class="col-4">
+        <div class="col-lg-4">
           <img src="../assets/people/farsi.png" style="width: 153px; border-radius: 20%">
           <h5>Saleh Farsi</h5>
           <h5>far30.1999@gmail.com </h5>
           <h6>B.Sc. Mechanical Engineering, Ferdowsi University, Mashhad, Iran, <br> (2017/present)</h6>
           <h6>Skills: Mechanical design, Machine learning and Machine Vision, Simulation (COMSOL, Abaqus, Adams) </h6>
         </div>
-        <div class="col-4"> <img
+        <div class="col-lg-4"> <img
           <img src="../assets/people/rezaei.png" style="width: 205px; border-radius: 20%">
           <h5>Moein Rezaei</h5>
           <h5>rezaei@UnboundGamestudio.com</h5>
