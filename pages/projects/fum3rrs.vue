b98e02993df8f8ea5002de5115ee1b66b38771d7
soroushatashin <s.atasheen@yahoo.com>
2020-12-29 23:54:54 +0330

    new

---
+++ pages/projects/fum3rrs.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "fum3rrs"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/projects/fumhexa.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "fumhexa"
+}
+</script>
+
+<style scoped>
+
+</style>
