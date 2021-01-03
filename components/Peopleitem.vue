8cd3926c8d5c10181146087ac940a93638483856
soroushatashin <75244812+soroushatashin@users.noreply.github.com>
2021-01-03 14:20:01 +0330

    Peopleitem is added

---
+++ components/Peopleitem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-lg-4" v-bind:href="href">
+   <img :src="` /_nuxt/assets/people/${img}`" style="height: 180px; border-radius: 20%">
+    <h5>{{ name }}</h5>
+    <h5>{{grade}} <br> <h6>{{email}}</h6> </h5>
+
+
+    <h6>Research Interests/Specializations: {{research}} </h6>
+    <a :href="`${linkedin}`" target="_blank"> <fa :icon="['fab','linkedin']" style="margin-right: 33px; color: #0a66c2;   "/> </a>
+  </div>
+</template>
+<script>
+export default {
+name: "Peopleitem",
+      props:{
+      img:String,
+      name:String,
+      grade:String,
+      email:String,
+      research:String,
+      linkedin:String,
+      },
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/projects/fumdelta.vue
+++ pages/projects/fumdelta.vue
@@ -2,15 +2,16 @@
   <div id="page-wrapper">
     <div class="content container">
       <h2>Fum Delta</h2>
-      <br>
-      <br>
+
       <div class="row">
         <div class="col-12 justify-content-center text-center mx-auto my-auto" >
-          <img src="../../assets/Robot.png" style="width: 30%; border-radius: 20%">
+          <img src="../../assets/ProjectPictures/delta.png" style="width: 40%; border-radius: 20%">
         </div>
       </div>
+      <br>
+      <br>
       <div class="row">
-        <p>The Semi industrial 3PSP is of type 3PSP parallel robot. It can run in any of the three X,Y,Z,ϑ,φ,λ degrees of freedom, prominently used for orientating tools and solar panels. The robot has an open control architecture suitable for research applications. </p>
+        <p> A parallel robot with three parallelogram arms for picking and packaging with high acceleration and speed capability. </p>
       </div>
 
     </div>
